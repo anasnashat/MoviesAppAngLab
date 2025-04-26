29ad4fd46874494bc4e4e906eb1c27138b7ed38c
anas nashat <aanasht@gmail.com>
2025-04-26 04:14:14 +0300

    Add PeopleComponent to fetch and display people list
    Introduced a new PeopleComponent with styles, template, and logic to display a list of people or appropriate error/loading messages. Integrated with MoviesService to fetch data and handle subscriptions for cleanup.

---
+++ src/app/people/people.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MoviesService} from '../movies.service';
+import {DecimalPipe} from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MovieCardComponent} from '../all-movies/movie-card/movie-card.component';
+import {Movie} from '../models/movie.model';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-people',
+  standalone: true,
+  imports: [
+    MovieCardComponent,
+    RouterLink,
+    RouterLinkActive,
+    DecimalPipe
+  ],
+  templateUrl: './people.component.html',
+  styleUrl: './people.component.css'
+})
+export class PeopleComponent implements OnInit, OnDestroy {
+  people: Movie[] = [];
+  isLoading = true;
+  error = '';
+  private subscription: Subscription | null = null;
+
+  constructor(private _moviesService: MoviesService) { }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.subscription = this._moviesService.getPeople().subscribe({
+      next: (data) => {
+        this.people = data.results;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load people. Please try again later.';
+        this.isLoading = false;
+        console.error(err);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
