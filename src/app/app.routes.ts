14a896da7e22287bfdd8b2f5ae8e779f6cc34151
anas nashat <aanasht@gmail.com>
2025-04-26 04:12:31 +0300

    Enhance routing and layout: add movie details and additional components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AllMoviesComponent} from './all-movies/all-movies.component';
+import {MovieDetailsComponent} from './all-movies/movie-details/movie-details.component';
+import {MoviesComponent} from './movies/movies.component';
+import {PeopleComponent} from './people/people.component';
+import {TVComponent} from './tv/tv.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path:'all', component:AllMoviesComponent}
+  {
+    path: 'all',
+    component: AllMoviesComponent,
+  },
+  { path: 'all/movie/:id/:type', component: MovieDetailsComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'tv', component: TVComponent },
 ];
-
