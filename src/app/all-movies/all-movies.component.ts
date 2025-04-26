7030b6bab497707333749caf51e402fe27a90f26
anas nashat <aanasht@gmail.com>
2025-04-26 04:12:13 +0300

    Implement loading and error handling in all-movies component

--- src/app/all-movies/all-movies.component.ts
+++ src/app/all-movies/all-movies.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MoviesService} from '../movies.service';
+import {MovieCardComponent} from './movie-card/movie-card.component';
+import {Movie} from '../models/movie.model';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-all-movies',
-  imports: [],
+  standalone: true,
+  imports: [
+    MovieCardComponent
+  ],
   templateUrl: './all-movies.component.html',
   styleUrl: './all-movies.component.css'
 })
-export class AllMoviesComponent {
+export class AllMoviesComponent implements OnInit, OnDestroy {
+  movies: Movie[] = [];
+  isLoading = true;
+  error = '';
+  private subscription: Subscription | null = null;
 
+  constructor(private _moviesService: MoviesService) { }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.subscription = this._moviesService.getAllMovies().subscribe({
+      next: (data) => {
+        this.movies = data.results;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load movies. Please try again later.';
+        this.isLoading = false;
+        console.error(err);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
