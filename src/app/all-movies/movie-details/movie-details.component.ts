f7b12177ec966899b157a1d816547c094fe8bb19
anas nashat <aanasht@gmail.com>
2025-04-26 04:13:46 +0300

    Add MovieDetailsComponent with loading, details, and error states
    Introduced a standalone MovieDetailsComponent to display movie, TV show, or person-specific details. Includes dynamic content rendering based on type and error handling for invalid data. Added corresponding HTML, CSS, TypeScript, and spec files for functionality and testing.

---
+++ src/app/all-movies/movie-details/movie-details.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MoviesService} from '../../movies.service';
+import {ActivatedRoute} from '@angular/router';
+import {DatePipe, NgTemplateOutlet} from '@angular/common';
+import {Movie} from '../../models/movie.model';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-movie-details',
+  standalone: true,
+  imports: [
+    DatePipe,
+    NgTemplateOutlet
+  ],
+  templateUrl: './movie-details.component.html',
+  styleUrl: './movie-details.component.css'
+})
+export class MovieDetailsComponent implements OnInit, OnDestroy {
+  movieId: number | undefined;
+  type: string | null | undefined;
+  movie: Movie | null = null;
+  isLoading = true;
+  error = '';
+  private subscription: Subscription | null = null;
+
+  constructor(
+    private _moviesService: MoviesService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    this.movieId = idParam ? +idParam : undefined;
+    this.type = this.route.snapshot.paramMap.get('type');
+
+    if (this.movieId && this.type) {
+      this.loadMovieDetails();
+    } else {
+      this.error = 'Invalid movie ID or type.';
+      this.isLoading = false;
+    }
+  }
+
+  loadMovieDetails() {
+    this.isLoading = true;
+    if (this.movieId && this.type) {
+      this.subscription = this._moviesService.getMovieDetails(this.movieId, this.type).subscribe({
+        next: (data) => {
+          this.movie = data;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          this.error = 'Failed to load details. Please try again later.';
+          this.isLoading = false;
+          console.error(err);
+        }
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
