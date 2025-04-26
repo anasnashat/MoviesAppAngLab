87f0b11318bfdde0afc90a66063daf0e5897c33e
anas nashat <aanasht@gmail.com>
2025-04-26 04:12:45 +0300

    Add carousel and welcome section to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
